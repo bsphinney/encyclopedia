00984940f870c68dda28878319d2a5273aaf84cd
Brett Phinney <bsphinney@ucdavis.edu>
2020-02-27 10:43:36 -0800

    Fist version

---
+++ encyclopedia_subset_fasta.R
@@ -0,0 +1,20 @@
+# create a subset fasta from Fragpipe/MSFragger results and initial database
+
+library('tidyverse')
+library("seqinr")
+
+# setwd to your dir. Should contain the .fas database and the fragpipe/MSfragger
+# protein.tsv output file
+setwd("E:\\Data\\encyclopedia")
+
+# read fasta used in the MSFragger search. Change this to the one you used. 
+fasta <- read.fasta("UP000005640_9606_pluscrap.fasta", seqtype = "AA",as.string = TRUE, set.attributes = FALSE)
+
+# read fragpipe/msfragger protein results file
+encyclopedia_results <- read_tsv("allmax2020_encyclopedia_concatenated_results.txt")
+
+# create subset fasta that only includes MSFragger results (usually the FDR is 1%)
+encyclopedia.subset<- names(fasta) %in% encyclopedia_results$proteinIds
+encyclopedia.subset.fasta <- fasta[encyclopedia.subset]
+
+write.fasta(sequences = encyclopedia.subset.fasta, names = names(encyclopedia.subset.fasta), nbchar = 80, file.out = "encyclopedia.subset.fasta")
